f371ad85e0327b3bd21e5c80fe2e25211ecaa53a
Ekta <dudh0004@algonquinlive.com>
2021-03-25 22:56:46 -0400

    Completed Babel js exercise

---
+++ js/app.js
@@ -0,0 +1,49 @@
+function getData(ev) {
+    let url = `https://randomfox.ca/floof/`;
+    fetch(url)
+    .then(function (response) {
+        if(response.ok){
+            console.log('good response');
+        return response.json();
+        }
+        else{
+        console.log('bad response', response.status);
+        throw new Error('BAD RESPONSE');
+        }
+    })
+    .then(function (data) {
+        console.log(data); 
+        setData(data);  
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+}
+
+function setData({image, link}) {
+
+    console.log(image);
+    console.log(link);
+    let div = document.getElementById('lorem');
+    let img = document.createElement('img');
+    img.src = image;
+    img.alt = link;
+    div.appendChild(img);
+    setData2();
+
+}
+
+async function setData2(){
+    let b = await asyncData(3,4);
+    console.log(b);
+}
+
+function asyncData(x,y){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x*y);
+        }, 1000); 
+    });
+}
+
+getData();
